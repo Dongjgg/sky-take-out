df346f08d2a0da8d8fb592d1cf52527524b6a9b9
Dj <1132580253@qq.com>
2023-07-31 11:29:37 +0800

    编辑员工业务代码开发，2023年7月31日

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 @Data
 public class EmployeeDTO implements Serializable {
 
+    private Long id;
     private String username;
 
     private String name;
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -30,4 +30,14 @@ public interface EmployeeMapper {
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void update(Employee employee);
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
+
+
 }
