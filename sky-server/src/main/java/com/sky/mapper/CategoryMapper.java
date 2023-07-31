e1d2094c8679b593d6b09ad8f7ccaf8410f7e22c
Dj <1132580253@qq.com>
2023-07-31 17:20:10 +0800

    公共字段自动填充代码实现，2023年7月31日

--- sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
+++ sky-server/src/main/java/com/sky/mapper/CategoryMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -19,7 +21,14 @@ public interface CategoryMapper {
     @Insert("insert into category(type, name, sort, status, create_time, update_time, create_user, update_user)" +
             " VALUES" +
             " (#{type}, #{name}, #{sort}, #{status}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Category category);
+    /**
+     * 根据id修改分类
+     * @param category
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Category category);
 
     /**
      * 分页查询
@@ -35,11 +44,6 @@ public interface CategoryMapper {
     @Delete("delete from category where id = #{id}")
     void deleteById(Long id);
 
-    /**
-     * 根据id修改分类
-     * @param category
-     */
-    void update(Category category);
 
     /**
      * 根据类型查询分类
